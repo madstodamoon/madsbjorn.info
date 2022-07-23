5a2d08cd1c86f2c868bd5ec8f5a19d6a4df35d93
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-07-23 23:58:28 +0200

    Letter spacing

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Studio Mads Bjørn Christiansen</h1>
-      <h1>All enquiries to studio@madsbjorn.info</h1>
+      <h1>
+        Studio Mads Bjørn Christiansen. All enquiries to studio@madsbjorn.info
+      </h1>
     </div>
   );
 }
