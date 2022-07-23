3688ffea3618c4a58d35563a17f4f9899a6d7627
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-07-23 23:10:22 +0200

    Text content update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Coming soon</h1>
+      <h1>Studio Mads Bjørn Christiansen</h1>
+      <h1>All enquiries to studio@madsbjorn.info</h1>
     </div>
   );
 }
