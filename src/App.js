d7e34c5d91723562586dba097102110bea58ed9b
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-07-26 21:55:20 +0200

    Updated text content

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ export default function App() {
     <div className="App">
       <h1>
         {" "}
-        <a> Studio </a> <a> Mads </a> <a> Bjørn </a> <a> Christiansen</a>.{" "}
-        <a> All </a> <a> enquiries </a> <a> to </a>{" "}
+        <a> Mads </a> <a> Bjørn </a> <a> Christiansen</a> <a> is </a>{" "}
+        <a> an </a> <a> independent </a> <a> architect </a> <a> engaged </a>{" "}
+        <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>{" "}
+        <a> of </a> <a> virtual </a> <a> and </a> <a> build </a>{" "}
+        <a> envirronments </a>. <a> All </a> <a> enquiries </a> <a> to </a>{" "}
         <a onClick={() => (window.location = "mailto:studio@madsbjorn.info")}>
           studio@madsbjorn.info
         </a>
