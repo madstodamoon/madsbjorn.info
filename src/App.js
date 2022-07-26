916d0e5a6ef9c44ec91d28ebc52c30606069720c
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-07-26 22:25:32 +0200

    Text size

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         <a> Mads </a> <a> Bjørn </a> <a> Christiansen</a> <a> is </a>{" "}
         <a> an </a> <a> independent </a> <a> architect </a> <a> engaged </a>{" "}
         <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>{" "}
-        <a> of </a> <a> virtual </a> <a> and </a> <a> build </a>{" "}
+        <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>{" "}
         <a> envirronments </a>. <a> All </a> <a> enquiries </a> <a> to </a>{" "}
         <a onClick={() => (window.location = "mailto:studio@madsbjorn.info")}>
           studio@madsbjorn.info
