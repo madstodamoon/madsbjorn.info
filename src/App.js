f06e234d30ed5f4361d6662c6313746d9376c6ea
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-08-16 12:31:00 +0200

    Missing space

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ export default function App() {
     <div className="App">
       <h1>
         {" "}
-        <a> Mads </a> <a> Bjørn </a> <a> Christiansen</a> <a> is </a>{" "}
-        <a> an </a> <a> independent </a> <a> architect </a> <a> engaged </a>{" "}
-        <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>{" "}
-        <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>{" "}
-        <a> envirronments </a>. <a> All </a> <a> enquiries </a> <a> to </a>{" "}
+        <a> Mads </a> <a> Bjørn </a> <a> Christiansen </a> <a> is </a>
+        <a> an </a> <a> independent </a> <a> architect </a> <a> engaged </a>
+        <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>
+        <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>
+        <a> envirronments </a> . <a> All </a> <a> enquiries </a> <a> to </a>
         <a onClick={() => (window.location = "mailto:studio@madsbjorn.info")}>
           studio@madsbjorn.info
         </a>
