6bbe82cf62eddf2ddbede089654696ae1be51147
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-10-09 17:13:44 -0500

    Device padding

--- src/text-animation/FootnoteAnimation.js
+++ src/text-animation/FootnoteAnimation.js
@@ -2,5 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 export default function TextAnimation() {
-  return <footer> * footnote test long sentence line shift adjustment </footer>;
+  return (
+    <footer>
+      {" "}
+      * Test title (2022) <br /> Long escription test for long instance.{" "}
+    </footer>
+  );
 }
