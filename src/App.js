c285b90acbcd6e601ae09292abe85ecc3e552663
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-10-09 02:06:00 -0500

    Test footnote

--- src/App.js
+++ src/App.js
@@ -8,31 +8,31 @@ import FootnoteAnimation from "./text-animation/FootnoteAnimation.js";
 export default function App() {
   return (
     <>
-      <div className="App">
-        <h1>
+      <div className="Footnote">
+        <div>
           {" "}
-          <a> Mads </a> <a> Bjørn </a> <a> Christiansen </a> <a> is </a>
-          <a> an </a> <a> independent </a> <a> architect </a> <a> engaged </a>
-          <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>
-          <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>
-          <a> envirronments </a> .
-          <br />
-          <a> All </a> <a> inquiries </a> <a> to </a>{" "}
-          <span
-            class="link-text"
-            onClick={() => (window.location = "mailto:studio@madsbjorn.info")}
-          >
-            studio@madsbjorn.info
-          </span>
-        </h1>
+          <FootnoteAnimation />{" "}
+        </div>
       </div>
 
       <>
-        <div className="Footnote">
-          <p>
+        <div className="App">
+          <h1>
             {" "}
-            <FootnoteAnimation />{" "}
-          </p>
+            <a> Mads </a> <a> Bjørn </a> <a> Christiansen </a> <a> is </a>
+            <a> an </a> <a> independent </a> <a> architect </a> <a> engaged </a>
+            <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>
+            <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>
+            <a> envirronments </a> .
+            <br />
+            <a> All </a> <a> inquiries </a> <a> to </a>{" "}
+            <span
+              class="link-text"
+              onClick={() => (window.location = "mailto:studio@madsbjorn.info")}
+            >
+              studio@madsbjorn.info
+            </span>
+          </h1>
         </div>
       </>
     </>
--- src/animation/FootnoteAnimation.js
+++ src/animation/FootnoteAnimation.js
@@ -2,5 +2,5 @@ import React from "react";
 import styled from "styled-components";
 
 export default function FootnoteAnimation() {
-  return <div> Test footnote text </div>;
+  return <div> Footnote </div>;
 }
