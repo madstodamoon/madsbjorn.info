9daa687071a577141af32176f867161a3df45670
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-10-09 00:53:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
-import "./responsive.css";
+import "./styles/styles.css";
+import "./styles/responsive.css";
+import "./animation/FootnoteAnimation.js";
+import FootnoteAnimation from "./animation/FootnoteAnimation.js";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>
-        {" "}
-        <a> Mads </a> <a> Bjørn </a> <a> Christiansen </a> <a> is </a>
-        <a> an </a> <a> independent </a> <a> architect </a> <a> engaged </a>
-        <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>
-        <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>
-        <a> envirronments </a> .
-        <br />
-        <a> All </a> <a> inquiries </a> <a> to </a>{" "}
-        <span
-          class="link-text"
-          onClick={() => (window.location = "mailto:studio@madsbjorn.info")}
-        >
-          studio@madsbjorn.info
-        </span>
-      </h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>
+          {" "}
+          <a> Mads </a> <a> Bjørn </a> <a> Christiansen </a> <a> is </a>
+          <a> an </a> <a> independent </a> <a> architect </a> <a> engaged </a>
+          <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>
+          <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>
+          <a> envirronments </a> .
+          <br />
+          <a> All </a> <a> inquiries </a> <a> to </a>{" "}
+          <span
+            class="link-text"
+            onClick={() => (window.location = "mailto:studio@madsbjorn.info")}
+          >
+            studio@madsbjorn.info
+          </span>
+        </h1>
+      </div>
+
+      <>
+        <div className="Footnote">
+          <p>
+            {" "}
+            <FootnoteAnimation />{" "}
+          </p>
+        </div>
+      </>
+    </>
   );
 }
