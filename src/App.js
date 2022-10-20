530f4ac091550431051ab3af3c14a5559ab0beef
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-10-20 00:34:58 -0500

    "architect changes state on click

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import "./responsive.css";
-import ReactCSSTransitionGroup from "react-transition-group";
+import { useState } from "react";
 
 //Prevents scrolling. Set to "auto" if needed
+
 document.body.style.overflow = "hidden";
 
 // Export
 
 export default function App() {
-  //Changes status bar color on devices
+  //onclick events
+
+  const initialText = "architect";
+  const [buttonTextArchitect, setButtonText] = useState(initialText);
+
+  function handleClick() {
+    setButtonText("architect*");
+
+    setTimeout(() => {
+      setButtonText(initialText);
+    }, 20000); // 👈️ change text back after 1 second
+  }
+
+  //Changes status bar color on devices?
 
   // Content starts here
 
@@ -21,7 +35,10 @@ export default function App() {
           <h1>
             {" "}
             <a> Mads </a> <a> Bjørn </a> <a> Christiansen </a> <a> is </a>
-            <a> an </a> <a> independent </a> <a> architect </a>
+            <a> an </a> <a> independent </a>
+            <span class="link-text" onClick={handleClick}>
+              {buttonTextArchitect}
+            </span>
             <a> engaged </a>
             <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>
             <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>
