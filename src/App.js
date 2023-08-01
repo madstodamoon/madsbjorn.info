c04c878ade45f18d04056ea57dec8ffb08547cd6
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2023-08-01 22:41:55 +0200

    Text update

--- src/App.js
+++ src/App.js
@@ -12,11 +12,15 @@ document.body.style.overflow = "hidden";
 
 export default function App() {
   const [isFooterVisible, setIsFooterVisible] = useState(false);
-  const [buttonTextArchitect, setButtonText] = useState("architect");
+  const [buttonTextArchitect, setButtonText] = useState(
+    "Mads Bjørn Christiansen"
+  );
 
   function handleClick() {
     setButtonText(
-      buttonTextArchitect === "architect" ? "architect*" : "architect"
+      buttonTextArchitect === "Mads Bjørn Christiansen"
+        ? "Mads Bjørn Christiansen*"
+        : "Mads Bjørn Christiansen"
     );
     setIsFooterVisible(!isFooterVisible);
   }
@@ -29,17 +33,17 @@ export default function App() {
         <div className="App">
           <h1>
             {" "}
-            <a> Mads </a> <a> Bjørn </a> <a> Christiansen </a> <a> is </a>
-            <a> an </a> <a> independent </a>
             <span class="link-text" onClick={handleClick}>
               {buttonTextArchitect}
             </span>
-            <a> engaged </a>
-            <a> globally </a> <a> within </a> <a> the </a> <a> realms </a>
+            <a> conducts </a>
+            <a> an </a> <a> architectural </a> <a> practice </a>{" "}
+            <a> engaged </a> <a> independently </a> <a> and </a>{" "}
+            <a> collaboratively </a> <a> within </a> <a> the </a> <a> realm </a>{" "}
             <a> of </a> <a> virtual </a> <a> and </a> <a> built </a>
             <a> environments </a> .
             <br />
-            <a> All </a> <a> inquiries </a> <a> to </a>{" "}
+            <br />
             <span
               class="link-text"
               onClick={() => (window.location = "mailto:studio@madsbjorn.info")}
